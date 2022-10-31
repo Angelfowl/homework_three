5d924b4fbb0f20532486fb515a053ffbdcd69d83
Angelfowl <112956907+Angelfowl@users.noreply.github.com>
2022-10-31 18:32:07 -0400

    no changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# homework_three
+
+http://in-info-web4.informatics.iupui.edu/~aeh5/homework_three/index.html
---
+++ app/app.js
@@ -0,0 +1,56 @@
+const onClick = (event) => {
+    console.log(event.srcElement.id);
+    alert(event.srcElement.id);
+  };
+
+  window.addEventListener('click', onClick);
+
+
+
+    function changeRoute() {
+        let hashTag = window.location.hash;
+        let pageID = hashTag.replace("#", "");
+        console.log(hashTag + " " + pageID);
+    
+        if (pageID != ""){
+        $.get(`pages/${pageID}/${pageID}.html`, function(data){
+            // console.log("data " + data);
+            $("#app").html(data);
+        });
+    
+    }else {
+        $.get(`pages/home/home.html`, function(data){
+            // console.log("data " + data);
+            $("#app").html(data);
+        });
+
+    }
+    };
+
+
+
+function initListeners() {
+
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
+
+    $(".bars").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+    $(".links a").click(function(e){
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+
+}
+
+
+
+
+
+$(document).ready(function(){
+    initListeners();
+})
